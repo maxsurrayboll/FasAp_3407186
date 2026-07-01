2d2dfc36a90afd1e248f62be8b3beab886920b01
maxsurrayboll <banquetperezesteban02@gmail.com>
2026-07-01 06:37:49 -0500

    actualizacion conexion_bd.py

--- app/conexion_bd.py
+++ app/conexion_bd.py
@@ -1,22 +1,25 @@
 from sqlmodel import SQLModel, Session, create_engine
+from fastapi import APIRouter, HTTPException, FastAPI
 from typing import Annotated
 from fastapi import Depends
 
 
 nombre_bd = "bd_clientes_3407186.sqlite3"
-
-# conexion a la bd, con una URL (direccion)
 url_bd = f"sqlite:///{nombre_bd}"
 
-# motor de base de datos
+#motor de la BD
 motor_db = create_engine(url_bd)
 
+#definir el metodo para crear las tablas
+def crear_tablas(app: FastAPI):
+    SQLModel.metadata.create_all(motor_db)
+    yield
 
-# obtener sesion en la bd sqlite
+#definir el metodo para crear las tablas
 def obtener_sesion():
     with Session(motor_db) as mi_sesion:
         yield mi_sesion
 
-
-# Definir la dependencia, y esto registra mi sesion
+#denominado inyeccion de dependencias.
+#registrar la sesion como dependencia.
 Sesion_dependencia = Annotated(Session, Depends(obtener_sesion)) 
